5b906a4c36eb861e12f2cbd1620b2a4299db0642
kirtan modi <kirtan.modi@appgambit.com>
2023-07-05 18:58:18 +0530

    third

--- src/App.js
+++ src/App.js
@@ -1,25 +1,138 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Container, TextField, Button, Table, TableHead, TableBody, TableCell, TableRow } from '@mui/material';
 
-function App() {
+const API_URL = 'http://localhost:3000/data';
+
+const App = () => {
+  const [data, setData] = useState([]);
+  const [formData, setFormData] = useState({ id: '', name: '', age: '', email: '', city: '' });
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(API_URL);
+      setData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const handleAddData = async () => {
+    try {
+      const response = await axios.post(API_URL, formData);
+      setData((prevData) => [...prevData, response.data]);
+      setFormData({ id: '', name: '', age: '', email: '', city: '' });
+    } catch (error) {
+      console.error('Error adding data:', error);
+    }
+  };
+
+  const handleDeleteData = async (id) => {
+    try {
+      await axios.delete(`${API_URL}/${id}`);
+      setData((prevData) => prevData.filter((item) => item.id !== id));
+    } catch (error) {
+      console.error('Error deleting data:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="md" sx={{ marginTop: '2rem' }}>
+      <form onSubmit={handleAddData}>
+        <TextField
+          label="ID"
+          name="id"
+          value={formData.id}
+          onChange={handleInputChange}
+          fullWidth
+          margin="normal"
+          variant="outlined"
+        />
+        <TextField
+          label="Name"
+          name="name"
+          value={formData.name}
+          onChange={handleInputChange}
+          fullWidth
+          margin="normal"
+          variant="outlined"
+        />
+        <TextField
+          label="Age"
+          name="age"
+          value={formData.age}
+          onChange={handleInputChange}
+          fullWidth
+          margin="normal"
+          variant="outlined"
+        />
+        <TextField
+          label="Email"
+          name="email"
+          value={formData.email}
+          onChange={handleInputChange}
+          fullWidth
+          margin="normal"
+          variant="outlined"
+        />
+        <TextField
+          label="City"
+          name="city"
+          value={formData.city}
+          onChange={handleInputChange}
+          fullWidth
+          margin="normal"
+          variant="outlined"
+        />
+        <Button type="submit" variant="contained" color="primary" sx={{ marginTop: '1rem' }}>
+          Add Data
+        </Button>
+      </form>
+      <Table sx={{ marginTop: '2rem' }}>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Age</TableCell>
+            <TableCell>Email</TableCell>
+            <TableCell>City</TableCell>
+            <TableCell>Action</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((item) => (
+            <TableRow key={item.id}>
+              <TableCell>{item.id}</TableCell>
+              <TableCell>{item.name}</TableCell>
+              <TableCell>{item.age}</TableCell>
+              <TableCell>{item.email}</TableCell>
+              <TableCell>{item.city}</TableCell>
+              <TableCell>
+                <Button
+                  variant="contained"
+                  color="error"
+                  onClick={() => handleDeleteData(item.id)}
+                >
+                  Delete
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Container>
   );
-}
+};
 
 export default App;
